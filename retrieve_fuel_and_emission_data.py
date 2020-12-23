f277ee729f3657d526c19f55a2b3190bc978e3c7
Araz B. Makoo <araz100@gmail.com>
2020-12-23 10:10:29 +0100

    Changed to new format for production data

--- retrieve_fuel_and_emission_data.py
+++ retrieve_fuel_and_emission_data.py
@@ -3,28 +3,54 @@ import json
 import requests
 from requests.utils import requote_uri
 
-VALID_AREAS = ['DK']
+VALID_AREAS = ['DK1', 'DK2']
 YEAR = '2019'
+
+GROUP_FUELS = {
+    "Offshore": "Vind",
+    "Onshore": "Vind",
+    "Solceller": "Sol",
+    "Sol": "Sol",
+    "Anden VE": "Vandkraft",
+    "Vandkraft": "Vandkraft",
+    "Biogas": "Biomasse",
+    "Træ_mm": "Biomasse",
+    "Halm": "Biomasse",
+    "Affald": "Affald",
+    "Naturgas": "Naturgas",
+    "Brunkul": "Kul og Olie",
+    "Kul": "Kul og Olie",
+    "Fuelolie": "Kul og Olie",
+    "Gasolie": "Kul og Olie",
+    "Olie": "Kul og Olie",
+    "Atomkraft": "Atomkraft"
+}
+
+FUEL_TYPES = list(set(GROUP_FUELS.values()))
 
 
 def retrieve_fuel_data():
-    url = 'https://www.energidataservice.dk/proxy/api/datastore_search_sql?sql=\
-          SELECT "HourUTC", "PriceArea", "DKFuelType", "ConsumedDK" \
-          FROM "declarationproductiontypeshour" \
-          WHERE "HourDK" >= \'2019-01-01\' AND "HourDK" <= \'2019-12-31\' \
+    url = f'https://www.energidataservice.dk/proxy/api/datastore_search_sql?sql=\
+          SELECT "HourUTC", "PriceArea", "ConnectedArea", "ProductionGroup", "Share" \
+          FROM "declconscoveragehour" \
+          WHERE "HourDK" >= \'{YEAR}-01-01\' AND "HourDK" <= \'{YEAR}-12-31\' \
           ORDER BY "HourUTC" ASC'
 
     response = requests.get(requote_uri(url))
     records = response.json()['result']['records']
 
+    for record in records:
+        record['ProductionGroup'] = GROUP_FUELS[record['ProductionGroup']]
+        record['PriceArea'] = record['PriceArea'].upper()
+
     return records
 
 
 def retrieve_emission_data():
-    url = 'https://www.energidataservice.dk/proxy/api/datastore_search_sql?sql=\
+    url = f'https://www.energidataservice.dk/proxy/api/datastore_search_sql?sql=\
            SELECT "HourUTC", "PriceArea", "Co2PerkWh" \
            FROM "declarationemissionhour" \
-           WHERE "HourDK" >= \'2019-01-01\' AND "HourDK" <= \'2019-12-31\' \
+           WHERE "HourDK" >= \'{YEAR}-01-01\' AND "HourDK" <= \'{YEAR}-12-31\' \
            ORDER BY "HourUTC" ASC'
 
     response = requests.get(requote_uri(url))
@@ -44,6 +70,8 @@ def calculate_kwh_per_hour(fuel_data):
 
         kwh_hourly[(hour, area)] = kwh_hourly.get((hour, area), 0) + consumed
 
+        fuel_types['']
+
         fuel_types.add(row['DKFuelType'])
 
     return list(fuel_types), kwh_hourly
@@ -57,40 +85,44 @@ def fill_missing_fuel_type_per_hour(fuel_data):
     for row in fuel_data:
         hour = row['HourUTC']
         area = row['PriceArea']
+        fuelType = row['ProductionGroup']
 
         if not hour in filled_fuel_data[area]:
             filled_fuel_data[area][hour] = {}
 
-        consumed = row['ConsumedDK'] * 1000 # convert from MWh to kWh
-        filled_fuel_data[area][hour][row['DKFuelType']] = filled_fuel_data[area][hour].get(row['DKFuelType'], 0) + consumed
+        filled_fuel_data[area][hour][fuelType] = filled_fuel_data[area][hour].get(fuelType, 0) + row['Share']
 
     return filled_fuel_data
 
 def convert_fuel_data(fuel_data):
-    fuel_types, kwh_hourly = calculate_kwh_per_hour(fuel_data)
-
     converted_data = {}
     for area in VALID_AREAS:
         converted_data[area] = {}
-        for fuel_type in fuel_types:
+        for fuel_type in FUEL_TYPES:
             converted_data[area][fuel_type] = []
 
     filled_fuel_data = fill_missing_fuel_type_per_hour(fuel_data)
 
     for area in VALID_AREAS:
+
         for hour in filled_fuel_data[area]:
-            for fuel_type in fuel_types:
+            test = 0
+            for fuel_type in FUEL_TYPES:
 
-                kwh = filled_fuel_data[area][hour].get(fuel_type, 0)
-                procentwise = kwh / kwh_hourly[(hour, area)]
+                share = filled_fuel_data[area][hour].get(fuel_type, 0)
 
-                converted_data[area][fuel_type].append({'HourUTC': hour,'ProcentwiseGross': procentwise})
+                test += share
+
+                converted_data[area][fuel_type].append({'HourUTC': hour,'Share': share})
+
+            if test != 1:
+                print(test)
 
     for area in VALID_AREAS:
-        for fuel_type in fuel_types:
+        for fuel_type in FUEL_TYPES:
             converted_data[area][fuel_type] = sorted(converted_data[area][fuel_type], key = lambda i: i['HourUTC'])
 
-    return fuel_types, converted_data
+    return converted_data
 
 
 def convert_emission_data(emission_data):
@@ -103,7 +135,7 @@ def convert_emission_data(emission_data):
             converted_data[area][emission_type] = []
 
     for row in emission_data:
-        area = row['PriceArea']
+        area =  "DK1" #row['PriceArea']
 
         for emission_type in emission_types:
             try:
@@ -131,10 +163,10 @@ def main():
     fuel_data = retrieve_fuel_data()
     emission_data = retrieve_emission_data()
 
-    fuel_types, converted_fuel_data = convert_fuel_data(fuel_data)
+    converted_fuel_data = convert_fuel_data(fuel_data)
     emission_types, converted_emission_data = convert_emission_data(emission_data)
 
-    write_to_file(emission_types, fuel_types, converted_emission_data, converted_fuel_data)
+    write_to_file(emission_types, FUEL_TYPES, converted_emission_data, converted_fuel_data)
 
 
 if __name__ == '__main__':
